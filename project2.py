1bb9e9fa7d48631d7b81ea4c18fc72c1e7ef105f
tommo593 <tomdevwork@gmail.com>
2023-11-19 14:39:45 +0000

    First commit

---
+++ project2.py
@@ -0,0 +1,32 @@
+# import story.txt
+
+with open("story.txt", "r") as f:
+    story = f.read()
+    
+words = []
+start_of_word = -1
+
+# finding any replaceable words in the story inside angle brackets <>
+
+target_start = "<"
+target_end = ">"
+    
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+        
+        if char == target_end and start_of_word != -1:
+            word = story[start_of_word: i + 1]
+            words.add(word)
+            start_of_word = -1
+            
+answers = {}
+
+for word in words:
+    answer = input("Enter a word for " + word + ": ")
+    answers[word] = answer
+    
+for word in words:
+    story = story.replace(word, answers[word])
+    
+    print(story)
